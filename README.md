fd274a88c2e0eae2e017de11433fa8d1e68ed17a
UserCDP <cojocaru.daniela@icloud.com>
2022-12-29 15:28:01 +0200

    Split the conversion of PDFs and TXT files into separate functions, detailed the README, added an example.

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# Converteste text scris in latina in text dubios (dar citibil pt cei care stiu)
+# Convertește text scris în latină în text scris in chirilică
 
-## Cum functioneaza
-Alege un document .PDF scris cu alfabet latin, iar programul va furniza un alt document .PDF, dar scris in alt alfabet.
+## Istoric
+Limba română a trecut destul de "recent" la scrierea în alfabet latin, așa că unele scrieri mai vechi sunt scrise in chirilică in original. Aceste texte sunt greu de citit și daca sunt scrise în latină așa că pentru a exersa citirea in aceasta sciere e nevoie de texte mai ușoare (și mai recente), dar nu mai exista așa ceva în ziua de astăzi.
+
+## Motivație
+Din păcate dintre toate limbile pe care le pot înțelege niciuna nu este scrisă in alt alfabet decat cel latin și nu prea poți fi sigur că citești corect dacă nu înțelegi ce citești așa că am creat acest program care "duce" textul din română scris normal cu câteva secole în urma, pe vremea cronicarilor...
+
+## Cum funcționează
+Alege un document `.PDF` scris cu alfabet latin, iar programul va furniza un document `.TXT`, cu scrierea modificată.
+
+---
+
+# Конвертеште техт скрис ын латинэ ын техт скрис ин ччириликэ
+
+## Историк
+Лимба ромынэ а трекут дестул де "рекент" ла скриереа ын алфабет латин, аша кэ унеле скриери маи веччи сунт скрисе ин ччириликэ ин оригинал. Акесте техте сунт греу де китит ши дака сунт скрисе ын латинэ аша кэ пентру а ехерса китиреа ин акеаста скиере е невоие де техте маи ушоаре (ши маи рекенте), дар ну маи ехиста аша кева ын зюуа де астэзи.
+
+## Мотивацие
+Дин пэкате динтре тоате лимбиле пе каре ле пот ынцелеге никюуна ну есте скрисэ ин алт алфабет декат кел латин ши ну преа поци фи сигур кэ китешти корект дакэ ну ынцелеги ке китешти аша кэ ам креат акест програм каре "дуке" техтул дин ромынэ скрис нормал ку кытева секоле ын урма, пе времеа кроникарилор...
+
+## Кум функционеазэ
+Алеге ун документ `.PDF` скрис ку алфабет латин, яар програмул ва фурниза ун документ `.ТХТ`, ку скриереа модификатэ.
--- convert.py
+++ convert.py
@@ -11,9 +11,43 @@ ALPHABET = {
     'M': 'М', 'N': 'Н', 'O': 'О', 'P': 'П', 'Q': 'ЧУ',
     'R': 'Р', 'S': 'С', 'Ș': 'Ш', 'T': 'Т', 'Ț': 'Ц',
     'U': 'У', 'V': 'В', 'W': 'В', 'X': 'Х', 'Y': 'Ы', 'Z': 'З'
-} # Then we have symbols for 'IU', 'CH' and 'IA'Ю Ч Я
+} # Then we have symbols for 'IU'-Ю, 'CH'-Ч and 'IA'-Я
+
+def __main__():
+    """The main body of the program"""
+    Tk().withdraw()
+    filename = askopenfilename()
+    outfile = open(filename.split('.')[0] + '-ch.txt', 'w')
+    if filename.split('.')[1].upper() == 'PDF':
+        open_pdf(filename, outfile)
+    elif filename.split('.')[1].upper() == 'TXT':
+        open_txt(filename, outfile)
+    outfile.close()
+
+def open_pdf(filename, outfile):
+    """
+    A function that recieves the name of the file and the name
+    of the outfile and prints the converted text from the
+    infile to the outfile
+    """
+    pdf_reader = PyPDF2.PdfReader(open(filename, 'rb'))
+    n = len(pdf_reader.pages)
+    for k in range(n):
+        convert_text(pdf_reader.pages[k].extract_text(), outfile) # convert each page one by one
+
+def open_txt(filename, outfile):
+    file = open(filename, 'r')
+    for line in file:
+        convert_text(line, outfile)
+
 
 def convert_text(text, outfile):
+    """
+    A function that uses the ALPHABET dictionary to replace letters
+    Recieves as input the text that we want to convert and the name
+    of the file to which we want to write the new text.
+    Returns nothing
+    """
     n = len(text)
     words_per_line = 0
     for i in range(n):
@@ -49,28 +83,7 @@ def convert_text(text, outfile):
                 words_per_line += 1
 
             if words_per_line == 12:
-                outfile.write('\n')
+                #outfile.write('\n')
                 words_per_line = 0
 
-
-Tk().withdraw()
-filename = askopenfilename()
-outfile = open(filename.split('.')[0] + '-ch.txt', 'w')
-
-if filename.split('.')[1].upper() == 'PDF':
-    pdf_reader = PyPDF2.PdfReader(open(filename, 'rb'))
-
-    k = 0
-    while True:
-        try:
-            text = pdf_reader.pages[k].extract_text()
-        except IndexError:
-            break
-        convert_text(text, outfile)
-        k += 1
-
-elif filename.split('.')[1].upper() == 'TXT':
-    file = open(filename, 'r')
-    for line in file:
-        convert_text(line, outfile)
-outfile.close()
+__main__()
